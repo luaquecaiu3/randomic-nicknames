acf3e06c3d02054a0e6eabb38a08b14a7012cf97
luaquecaiu3 <luaquecaiu3@users.noreply.github.com>
2019-08-01 18:08:47 -0300

    a

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,68 @@ $(function() {
 $(function(){
 	$('input, textarea').placeholder();
 });
+
+//my functions
+var header = document.getElementById('header_banner'),
+resultText = document.getElementById('result_cell'),
+rtN_S = resultText.value.split(' '),
+rtN = rtN_S[0],
+rtS = rtN_S[1],
+spliter = ' ';
+
+function banner(){
+  setInterval(function(){
+    //header.innerHTML += randomicnamessliding;
+  }, 1000);
+}
+banner();
+function copiarNick(){
+  resultText.select();
+  document.execCommand('copy');
+  alert('Copiado para área de tranferencia:\n' + resultText.value);
+}
+
+function mudarSexF(){
+  rtN = 'feminino';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSexM(){
+  rtN = 'masculino';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSexU(){
+  rtN = 'unissex';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSobN(){
+  rtS = 'sobrenome';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSobAm(){
+  rtS = 'adjAmigável';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSobAg(){
+  rtS = 'adjAgressivo';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSepE(){
+  spliter = ' ';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSepS(){
+  spliter = '_';
+  resultText.value = rtN + spliter + rtS;
+}
+
+function mudarSepT(){
+  spliter = '-';
+  resultText.value = rtN + spliter + rtS;
+}
